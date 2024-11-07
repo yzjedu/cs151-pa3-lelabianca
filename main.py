208c9f6ec8a4d66a22153ece2fd89ee836efd9e3
leiflabianca <leif.labianca@gmail.com>
2024-11-07 18:56:59 -0500

    finished pa3

--- main.py
+++ main.py
@@ -1,1 +1,111 @@
-# Code goes here and DO NOT FORGET INTRO COMMENTS
+# Programmers:  Leif LaBianca
+# Course:  CS151, Professor Yalew
+# Due Date: November 8 2024
+# Programming Assignment:  PA3
+# Problem Statement:  outputs a given ASCII art based on user input
+# Data In: type of art requested, number of lines if applicable
+# Data Out:  outputed ASCII art
+# Credits: all original code
+
+#imports the random function
+import random
+
+#function "circle" is defined
+def circle():
+    # prints a circle made up of asterisks
+    print('         ******          ')
+    print('    *****      *****     ')
+    print('  ***              ***   ')
+    print('**                     **')
+    print('*                       *')
+    print('**                     **')
+    print('  ***              ***   ')
+    print('    *****      *****     ')
+    print('         ******          ')
+    print()
+
+#function "line" is defined
+def line():
+    #passthru variable is defined for loop cancellation
+    passthru = False
+    while not passthru:
+        #user inputs the number of lines they want outputted
+        line_count = input('Please enter the amount of lines you want to display: ')
+        if not line_count.isdigit():
+            print('This is an invalid input. Please express your count in the form of a positive whole number')
+        else:
+            #a counter is set up so that the loop will continue for the requested amount of lines
+            line_count = int(line_count)
+            current_count = 0
+            while current_count < line_count:
+                print('********************')
+                current_count += 1
+            print()
+            passthru = True
+
+#function "randomascii" is defined
+def randomascii():
+    #a variable is set to a random number from 0 to 2
+    design = random.randint(0,2)
+    #outputted ASCII depends on random number selected
+    if design == 0:
+        print('        ---------   ')
+        print('-------|     *   |--')
+        print('        ---------   ')
+        print('       /     |      ')
+        print('      /      |      ')
+        print()
+    elif design == 1:
+        print(f'{"|":<10}{"|":>10}')
+        print(f'{"|":<10}{"/ ":>9}')
+        print(f'{"|":<10}{"/  ":>8}')
+        print('-' * 15)
+        print()
+    elif design == 2:
+        print(f'{"<":>5}{">":<5}')
+        print(f'{"<<":>5}{">>":<5}')
+        print(f'{"<<<":>5}{">>>":<5}')
+        print(f'{"<<<<":>5}{">>>>":<5}')
+        print(f'{"<<<<<":>5}{">>>>>":<5}')
+        print(f'{"<<<<":>5}{">>>>":<5}')
+        print(f'{"<<<":>5}{">>>":<5}')
+        print(f'{"<<":>5}{">>":<5}')
+        print(f'{"<":>5}{">":<5}')
+        print()
+
+#variable "wholepassthru" serves as end marker for the loop
+wholepassthru = False
+print('Welcome to the ASCII art generator.\nThis machine can generate a circle, a set number of lines, or a random choice of 3 designs')
+while not wholepassthru:
+    #two passthrus are defined for the main function for error checking
+    passthru2 = False
+    passthru3 = False
+    while not passthru2:
+        #the user selects one of the options, and the relevent function is executed to print the ASCII
+        selected_output = input('What type of ASCII art would you like to generate?\nPlease express your answer in the form of the first letter: ')
+        if selected_output.lower() == 'c':
+            circle()
+            passthru2 = True
+        elif selected_output.lower() == 'l':
+            line()
+            passthru2 = True
+        elif selected_output.lower() == 'r':
+            randomascii()
+            passthru2 = True
+        else:
+            print('This input is invalid.\nExpress your answer in the form of the first letter of the option (c,l,r)\n')
+    while not passthru3:
+        #the user is prompted to answer whether or not they would like to generate another ASCII
+        continue_query = input('Would you like to generate another art piece?\nExpress your answer with y or n: ')
+        if continue_query.lower() == 'y':
+            print('Restarting...\n')
+            passthru3 = True
+        elif continue_query.lower() == 'n':
+            print('Thank you for using our art generator!')
+            passthru3 = True
+            wholepassthru = True
+        else:
+            print('This input is invalid. Please express your query with y or n\n')
+
+
+
--- reflection.md
+++ reflection.md
@@ -1,9 +1,20 @@
 # Reflection Document
 
-* Full Name :  
-* Student ID:  
+* Full Name :  Leif LaBianca
+* Student ID:  1928607
 
-
+In this lab, the procedure was greatly reduced, and we were tasked with only working on the coding portion of this project.
+The objective of this project was to create an ASCII art generating tool that would output a circle, a given number of lines, or one of three random designs.
+First, I wrote out the functions for the single circle and the input lines, which were simple enough as they were.
+Then, I tasked myself with creating the three ASCII arts from scratch using complex line formatting.
+On that topic, such functions were the main lesson to be taught in this project. Essentially the objective was to figure out what they were and what they could be used for.
+My first code iteration managed to output what I expected, so I was pleased with that result.
+This project did not implement test cases.
+The main problem I came across during this project was just coming up with the unique ASCII design, which to my admission dont actually look very good or interesting.
+Then again, I'd argue that a lot of ASCII art, unless its a coding marvel, like the spinning donut, aren't really in general.
+Some key takeaways I had from this project were not to be afraid of advanced string formating (it's not that complicated) and to let down all barriers when trying to creative.
+I think I ultimately learned what I was supposed to in this lab, as a result. Working as an individual in this project wasn't particularly gamechanging due to the small workload.
+As such, I don't have many comments to make of it other than that I enjoyed the autonomy I had in this project.
 
 
 
